688937fc20d196da6395a141303c78d88aa676ad
Macrina Lobo <macrina.lobo@gmail.com>
2019-11-26 05:32:55 -0500

    fixes based on feedback
    fixed :
    q1.py & cdf plot
    README explanation of insert size distribution and somatic alterations

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# NGS_assignment
- 
+# NGS assignment
+
+instructions to run
+
+make sure gatk, samtools are installed and paths are correct
+
+Run:
+
+python q1.py
+
+python q2.py
+
+solutions to assignment:
+
+Files for part 1:
+
+q1.py - code for question 1
+
+read_length_histogram.png - histogram for q1
+
+cumulative_distribution_of_2mer_frequencies.png - distribution for q2
+
+terminal_output_for_q1.txt - terminal output for q1
+
+Part 2
+
+Files for part 2:
+
+q2.py - python code for part 2
+
+INPUT: downloaded bam and bai file
+
+HG00096.chrom20.ILLUMINA.bwa.GBR.low_coverage.20120522.bam //excluded due to size
+
+HG00096.chrom20.ILLUMINA.bwa.GBR.low_coverage.20120522.bam.bai
+
+OUTPUT:
+
+HG00096.chrom20.ILLUMINA.bwa.GBR.low_coverage.20120522.sorted.bam - sorted bam //excluded due to size
+
+insert_size_distribution.pdf - distribution of insert sizes
+
+insert_size_information.txt - statistics + histogram bin values of insert sizes
+
+terminal_output_for_q2.txt - terminal output for q2
+
+
+STEPS:
+1) ftp file download
+2) Header view: samtools view -H <bamfile>, Fileview: samtools view <bamfile> | less - just to look inside the file
+3) sort: samtools sort ( in this case it appears its already sorted )
+4) get summary stats - samtools flagstat - for my understanding, to look at duplicate, unmapped reads
+5) Picard CollectInsertMetrics to get and plot insert size distribution for paired end mapped reads - unpaired and duplicate reads are excluded
+
+ASSUMPTIONS - documented in code as well
+    
+    - ASSUMPTION 1: fragment size approximately equals insert size
+    Here I ignore adapter length ( usually trimmed ) since actually fragment size = insert size + length of adapters at both ends
+    
+    - ASSUMPTION 2: insert size approximately equals TLEN ( field 9 of sam ) which I believe Picard CollectInsertMetrics uses for its plots and stats
+    This assumption would be violated in case of RNAseq data due to splicing and hence introns not being sequenced but that is not relevant here
+    
+    - ASSUMPTION 3: In some places it seems "insert size" is used interchangeably with "inner mate size" but not here.
+    I assume inner mate size to be TLEN - len(R1) - len(R2) and do not use it anywhere.
+
+NOTE:
+
+CollectInsertMetrics has some filters which are implicitly set and I chose not to change:
+1) duplicates are excluded, as they should be
+2) extreme insert sizes ( 10 deviations away from median ) are excluded 
+3) 1431742 read pairs out of 2931500 reads meet the criteria here
+
+Insert size and somatic alterations:
+
+Abnormal insert sizes can be a sign of somatic structural variation in those regions.
+For a typical standardized target size, the insert size distribution should be unimodal with mode around the target size.
+tails in the insert size distribution could be due to alignment errors / artifacts. However, multimodal insert size distributions
+are indicative of somatic structural variation ( eg deletion, duplication, insertion, inversion, translocation)
+
+
+
+
--- q1.py
+++ q1.py
@@ -8,13 +8,13 @@ def get_mid_5_mer_and_rev_compl(sequence):
     if seq_len <= 5:
         return sequence
     elif seq_len % 2 != 0:
-        midpoint_of_seq = int((seq_len - 1)/2)
-        mid_sequence = sequence[midpoint_of_seq-2:midpoint_of_seq+3]
+        midpoint_of_seq = int((seq_len - 1) / 2)
+        mid_sequence = sequence[midpoint_of_seq - 2:midpoint_of_seq + 3]
         return mid_sequence, get_reverse_complement(mid_sequence)
     else:  # never happens for this input since all sequence lengths are odd
-        midpoint_of_seq = seq_len/2 - 1  # lower mid point considered if even
+        midpoint_of_seq = seq_len / 2 - 1  # lower mid point considered if even
         lower_mid_sequence = sequence[midpoint_of_seq - 2:midpoint_of_seq + 3]
-        upper_mid_sequence = sequence[midpoint_of_seq-1:midpoint_of_seq+4]
+        upper_mid_sequence = sequence[midpoint_of_seq - 1:midpoint_of_seq + 4]
         # return comma separated 2 kmers for even length sequence
         return '{0},{1}'.format(lower_mid_sequence, upper_mid_sequence), '{0},{1}'.format(
             get_reverse_complement(lower_mid_sequence), get_reverse_complement(upper_mid_sequence))
@@ -28,7 +28,7 @@ def get_reverse_complement(sequence):
         if base not in complement_dict:  # For Ex: there are some 'Y' bases in the fasta
             # in this dataset it doesnt appear to happen for the mid 5 mer so ignore for now;
             # would need special handling for reverse complement of each non A,G,T,C entry
-            print("this is a weird base {} in sequence {}".format(base,sequence))
+            print("this is a weird base {} in sequence {}".format(base, sequence))
             sys.exit(1)
         else:
             complement_seq.append(complement_dict[base])
@@ -45,29 +45,29 @@ def get_all_sorted_two_mers():
     return two_mers
 
 
-def get_vector_c(sequence, line_number):
-    two_mer_list = get_all_sorted_two_mers()
+def get_vector_c(sequence, line_number, two_mer_list):
     vector_c = [0] * len(two_mer_list)
     for count in range(len(sequence) - 1):
         # print(count)
-        if sequence[count:count+2] not in two_mer_list:
-            print("ignored {} in vector_C count for line {} at position {} of fasta".format(sequence[count:count+2],
+        if sequence[count:count + 2] not in two_mer_list:
+            print("ignored {} in vector_C count for line {} at position {} of fasta".format(sequence[count:count + 2],
                                                                                             line_number, count))
         else:
-            vector_c_idx = two_mer_list.index(sequence[count:count+2])
+            vector_c_idx = two_mer_list.index(sequence[count:count + 2])
             vector_c[vector_c_idx] = vector_c[vector_c_idx] + 1
     return '\t'.join([str(mer_count) for mer_count in vector_c])
 
 
 def main():
-
     # QUESTION 1
 
     count = 0
     processed_read = {}
     read_lengths = []
     vector_c = []
-    with open("Sequencing_Analysis_Assignment.fasta","r") as fasta, open("output_1a","w") as f_out:
+    two_mer_list = get_all_sorted_two_mers()
+
+    with open("Sequencing_Analysis_Assignment.fasta", "r") as fasta, open("output_1a", "w") as f_out:
         for line in fasta:
             count = count + 1
             line = line.strip()
@@ -77,10 +77,10 @@ def main():
             else:  # base sequence
                 read_lengths.append(len(line))
                 processed_read['mid_5mer'], processed_read['reverse_comp_mid_5mer'] = get_mid_5_mer_and_rev_compl(line)
-                vector_c.append(get_vector_c(line, count))
+                vector_c.append(get_vector_c(line, count, two_mer_list))
                 f_out.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(processed_read['read_id'],
-                            read_lengths[-1], processed_read['mid_5mer'],
-                            processed_read['reverse_comp_mid_5mer'], vector_c[-1]))
+                                                               read_lengths[-1], processed_read['mid_5mer'],
+                                                               processed_read['reverse_comp_mid_5mer'], vector_c[-1]))
 
     # PLOTS
     num_bins = int(math.sqrt(len(read_lengths))) + 1
@@ -90,38 +90,38 @@ def main():
     plt.xlabel("read length")
     plt.ylabel("number of reads")
     plt.title("histogram of read lengths")
-    # plt.draw()
     fig1.savefig("read_length_histogram.png")
 
-    # plot of cdf
+    # compute and plot cdf
     fig2 = plt.figure("cumulative distribution of 2mer frequencies")
-    ax = fig2.add_axes([0.1, 0.1, 0.8, 0.8])
-    plt.xlabel("two-mer")
-    xtick_labels = get_all_sorted_two_mers()
-    ax.set_xticks(range(len(xtick_labels)))
-    ax.set_xticklabels(xtick_labels)
-    plt.ylabel("cumulative frequency")
-    plt.title("cumulative distribution of 2mer frequencies for each sequence")
+    plt.xlabel("fasta read")
+    plt.ylabel("2mer frequency cumulative distribution")
+    plt.title("cumulative distribution of 2mer frequencies")
 
+    # define seq_2mer_freq_f as a list containing 16 lists - one list for each 2mer
+    # Each of the 16 lists contains the corresponding 2mer frequency for each of the fasta reads.
+    seq_2mer_freq_f = [[] for cnt in range(len(two_mer_list))]
+    twomer_cum_freq = [0] * len(two_mer_list)  # stores cumulative freq for each two-mer
     for genomic_sig_c_str in vector_c:
         genomic_sig_c_list = genomic_sig_c_str.split('\t')
         pseudo_counts_p = [float(two_mer_cnt) + 1 for two_mer_cnt in genomic_sig_c_list]
         sum_l = sum(pseudo_counts_p)
-        seq_2mer_freq_f = [px/sum_l for px in pseudo_counts_p]
-        sequence_cdf = [seq_2mer_freq_f[0]]
-        for idx in range(1, len(seq_2mer_freq_f)):
-            sequence_cdf.append(seq_2mer_freq_f[idx] + sequence_cdf[idx-1])
-        # print(seq_2mer_freq_f, sequence_cdf)
-        plt.plot(sequence_cdf)
+        for twomer_idx in range(len(two_mer_list)):
+            freq_val = pseudo_counts_p[twomer_idx] / sum_l
+            twomer_cum_freq[twomer_idx] = twomer_cum_freq[twomer_idx] + freq_val
+            seq_2mer_freq_f[twomer_idx].append(freq_val)
 
-    # plt.draw()
+    # get cumulative distribution
+    twomer_cdf = []
+    for twomer_idx in range(len(two_mer_list)):
+        twomer_cdf.append([seq_2mer_freq_f[twomer_idx][0] / twomer_cum_freq[twomer_idx]])
+        for seq_idx in range(1, len(seq_2mer_freq_f[twomer_idx])):
+            twomer_cdf[twomer_idx].append(twomer_cdf[twomer_idx][seq_idx - 1] +
+                                          seq_2mer_freq_f[twomer_idx][seq_idx] / twomer_cum_freq[twomer_idx])
+        plt.plot(twomer_cdf[twomer_idx])
+
     fig2.savefig("cumulative_distribution_of_2mer_frequencies.png")
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
